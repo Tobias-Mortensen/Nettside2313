af75206e7287cff0c73d84de64048bdb54e2008d
Tobias-Mortensen <tomo@hopvgskole.no>
2026-02-28 01:00:54 +0100

    ee

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const app = express();
 
-const PORT = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-    // __dirname points to the folder where app.js lives
-    const filePath = path.join(__dirname, 'index.html');
-
-    fs.readFile(filePath, (err, content) => {
-        if (err) {
-            res.writeHead(500);
-            res.end('Error loading index.html');
-        } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(content, 'utf-8');
-        }
-    });
+app.get("/", (req, res) => {
+  res.send("Server is working!");
 });
 
-server.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Running on port 3000");
 });
